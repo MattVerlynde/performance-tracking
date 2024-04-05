5663e71a4dbc0e186f3f9581cc10406486bb5d47
MattVerlynde <matthieu.verlynde@agroparistech.fr>
2024-04-05 09:47:02 +0200

    Change result directory in simulation metrics file

--- simulation_metrics_exec.sh
+++ simulation_metrics_exec.sh
@@ -17,7 +17,7 @@ echo "*************************************************"
 echo "Time stop: $t2"
 echo "*************************************************"
 
-echo "$(($(date -d "$t2" +%s%9N) - $(date -d "$t1" +%s%9N)))" >> execution_time
+echo "$(($(date -d "$t2" +%s%9N) - $(date -d "$t1" +%s%9N)))" >> results/execution_time
 
 query="data=from(bucket: \"telegraf_bucket\")
     |> range(start: ${t1}, stop: ${t2})
@@ -28,12 +28,13 @@ echo $query > query
 
 sudo docker cp query influxdb:/query
 
-sudo docker exec -it influxdb sh -c 'influx query -f query -r' > metrics_output
+mkdir -p results
+sudo docker exec -it influxdb sh -c 'influx query -f query -r' > results/metrics_output
 
 echo "*************************************************"
 echo "File metrics_output created"
 echo "*************************************************"
-head metrics_output
+head results/metrics_output
 echo "*************************************************"
 
 firefox -url "http://localhost:9090/d/edh1jtjp0b4lcb/38860f63-1c6f-5143-a2a7-cfc431003966?orgId=1&from=$(($(date -d "$t1" +%s%3N) - 1))&to=$(($(date -d "$t2" +%s%3N) + 1))"
