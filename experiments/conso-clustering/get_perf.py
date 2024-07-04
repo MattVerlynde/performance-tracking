fix auc calculation

experiments/conso-clustering/get_perf.py
@@ -16,6 +16,7 @@ import numpy as np
 from sklearn import metrics
 
 from utils_clustering import load_data
+from pyriemann.utils.mean import mean_riemann
 
 import argparse
 
@@ -24,9 +25,27 @@ import argparse
 # Plot data and results
 # ---------
 
-def get_scores(data_visualization, result):
+def get_scores(storage_path):
 
-    return metrics.silhouette_score(data_visualization, result, metric='euclidean')
+    with open(os.path.join(storage_path, "group_info.yaml"), 'r') as f:
+        paramYaml = yaml.load(f, Loader=yaml.FullLoader)
+
+    window = int(paramYaml['parameters']['--window'])
+
+    list_covar = sorted(os.listdir(os.path.join(storage_path, "covar")))
+    list_cluster = sorted(os.listdir(os.path.join(storage_path, "output")))
+    for file_covar, file_cluster in zip(list_covar, list_cluster):
+        covmat = np.load(os.path.join(storage_path, "covar", file_covar))
+        result = np.load(os.path.join(storage_path, "output", file_cluster))
+
+        covmat = (covmat[(window-1)//2:-(window-1)//2, (window-1)//2:-(window-1)//2, :])
+    
+    print(result.shape)
+    print(covmat.shape)
+    scores = {}
+    scores["silhouette"] = metrics.silhouette_score(covmat, result)
+
+    return scores
 
 ###############################################################################
 # References
@@ -48,19 +67,7 @@ if __name__ == "__main__":
     parser.add_argument("--storage_path", type=str, required=True)
     args = parser.parse_args()
 
-    with open(os.path.join(args.storage_path, "group_info.yaml"), 'r') as f:
-        paramYaml = yaml.load(f, Loader=yaml.FullLoader)
-
-    data_path = paramYaml['parameters']['--image'] + ".npy"
-    window = int(paramYaml['parameters']['--window'])
-    small_dataset = bool(paramYaml['parameters']['--small_dataset'])
-    n_clusters = int(paramYaml['parameters']['--n_clusters'])
-
-    data, data_visualization, X_image, Y_image, X_res, Y_res = load_data(data_path, n_clusters, window, small_dataset)
-    for file in os.listdir(os.path.join(args.storage_path, "output")):
-        result = np.load(os.path.join(args.storage_path, "output", file))
-
-        score = get_scores(data_visualization, result)
+    score = get_scores(args.storage_path)
     
     print("Done")
     print(f"Score is {score}")

experiments/conso-clustering/utils_clustering.py
@@ -97,17 +97,21 @@ def compute_clustering(pipelines_names, pipelines, data):
 
     print(f"\nStarting clustering with pipelines: {pipelines_names}")
     results = {}
+    covmats = {}
     for pipeline_name, pipeline in zip(pipelines_names, pipelines):
         print("-"*60)
         print(f"Pipeline: {pipeline_name}")
         pipeline.fit(data)
         preds = pipeline.named_steps["kmeans"].labels_
-        covmats = pipeline.named_steps["covariances"].transform(data)
+        covmats[pipeline_name] = pipeline.named_steps["sliding_window"].transform(data)
+        covmats[pipeline_name] = pipeline.named_steps["covariances"].transform(covmats[pipeline_name])
+        
+        print(f"Covmat shape: {covmats[pipeline_name].shape}")
         results[pipeline_name] = \
             pipeline.named_steps["sliding_window"].inverse_predict(preds)
         print("-"*60)
     print("Done")
-    return results
+    return results, covmats
 
 def clustering(data_path, n_clusters, window, small_dataset, estimator, n_jobs, max_iter, storage_path, number_run):
 
@@ -115,12 +119,17 @@ def clustering(data_path, n_clusters, window, small_dataset, estimator, n_jobs, 
 
     pipelines, pipelines_names = make_pipelines(window, estimator, n_clusters, n_jobs,  max_iter)
 
-    results = compute_clustering(pipelines_names, pipelines, data)
+    results, covmats = compute_clustering(pipelines_names, pipelines, data)
 
+    os.makedirs(os.path.join(storage_path, "output"), exist_ok=True)
     for pipeline_name, labels_pred in results.items():
         np.save(os.path.join(storage_path, "output", f"{pipeline_name}_clustering_results_{number_run}.npy"), labels_pred)
+    
+    os.makedirs(os.path.join(storage_path, "covar"), exist_ok=True)
+    for pipeline_name, cov in covmats.items():
+        np.save(os.path.join(storage_path, "covar", f"{pipeline_name}_covmats_{number_run}.npy"), cov)
 
-    return results
+    return results, covmats
 
 
 ###############################################################################

experiments/conso/get_perf.py
@@ -16,6 +16,7 @@ import numpy as np
 import argparse
 import plotly.express as px
 from scipy import integrate
+from sklearn import metrics
 from skimage.metrics import structural_similarity
 
 
@@ -52,27 +53,31 @@ def get_perf(storage_path, result):
     gapx, gapy = int((x_ref - x)/2), int((y_ref - y)/2)
     reference = reference[gapx:(x_ref-gapx), gapy:(y_ref-gapy)]
 
-    tpr = []
-    fpr = []
-    acc = []
+    # tpr = []
+    # fpr = []
+    # acc = []
     ref_thresh = np.where(reference != 0, 1, 0)
-    for t in np.linspace(0, 1, 100):
-        result_thresh = np.where(result >= t, 1, 0)
-        TP = np.sum(np.logical_and(ref_thresh == 1, result_thresh == 1))
-        FP = np.sum(np.logical_and(ref_thresh == 0, result_thresh == 1))
-        FN = np.sum(np.logical_and(ref_thresh == 1, result_thresh == 0))
-        TN = np.sum(np.logical_and(ref_thresh == 0, result_thresh == 0))
-        # print(f"TP: {TP}, FP: {FP}, FN: {FN}, TN: {TN}")
-        tpr.append(TP / (TP + FN))
-        fpr.append(FP / (FP + TN))
-        acc.append((TP + TN) / (TP + TN + FP + FN))
+    # max_val = np.max(result)
+    # for t in np.linspace(0, max_val, 101):
+    #     result_thresh = np.where(result >= t, 1, 0)
+    #     TP = np.sum(np.logical_and(ref_thresh == 1, result_thresh == 1))
+    #     FP = np.sum(np.logical_and(ref_thresh == 0, result_thresh == 1))
+    #     FN = np.sum(np.logical_and(ref_thresh == 1, result_thresh == 0))
+    #     TN = np.sum(np.logical_and(ref_thresh == 0, result_thresh == 0))
+    #     # print(f"TP: {TP}, FP: {FP}, FN: {FN}, TN: {TN}")
+    #     tpr.append(TP / (TP + FN))
+    #     fpr.append(FP / (FP + TN))
+    #     acc.append((TP + TN) / (TP + TN + FP + FN))
     
     #Calculating AUC
-    auc = abs(integrate.trapezoid(tpr, fpr))
+    # auc = abs(integrate.trapezoid(tpr, fpr))
+
+    fpr, tpr, threshold = metrics.roc_curve(y_true=ref_thresh.flatten(), y_score=result.flatten())
+    auc = metrics.auc(fpr, tpr)
 
     ssim = structural_similarity(ref_thresh, result, data_range=1.0)
 
-    return tpr, fpr, auc, ssim, acc
+    return tpr, fpr, auc, ssim
 
 if __name__ == "__main__":
 
@@ -82,23 +87,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for file in os.listdir(os.path.join(args.storage_path, "output")):
-        result = np.load(os.path.join(args.storage_path, "output", file))
+        result = (-1)*np.load(os.path.join(args.storage_path, "output", file))
 
-        tpr, fpr, auc, ssim, acc = get_perf(args.storage_path, result)
+        tpr, fpr, auc, ssim = get_perf(args.storage_path, result)
             
-        # print(f"TPR: {tpr}")
-        # print(f"FPR: {fpr}")
-        print(f"ACC: {np.mean(acc)}")
         print(f"AUC: {np.mean(auc)}")
         print(f"SSIM: {np.mean(ssim)}")
-        # print(f"Threshold: {threshold}")
 
         if args.plot:
             if not os.path.exists(os.path.join(args.storage_path, "plots")):
                 os.mkdir(os.path.join(args.storage_path, "plots"))
                 os.mkdir(os.path.join(args.storage_path, "plots/accuracy"))
                 os.mkdir(os.path.join(args.storage_path, "plots/roc"))
-            fig = px.line(x=fpr, y=tpr, title="ROC Curve", width=600, height=600)
+            fig = px.scatter(x=fpr, y=tpr, title="ROC Curve", width=600, height=600)
             fig.add_trace(px.line(x=[0, 1], y=[0, 1], color_discrete_sequence=["rgb(0, 0, 0)"]).data[0])
             fig.update_xaxes(title_text="False Positive Rate")
             fig.update_yaxes(title_text="True Positive Rate")
@@ -114,10 +115,12 @@ if __name__ == "__main__":
                 )
             )
             fig.write_html(os.path.join(args.storage_path, "plots/roc", "roc_curve_" + file[25:-4] + ".html"), include_mathjax='cdn')
+            fig.write_image(os.path.join(args.storage_path, "plots/roc", "roc_curve_" + file[25:-4] + ".png"))
             fig.show()
 
-            fig = px.line(x=np.linspace(0, 1, 100), y=acc, title="Accuracy", width=600, height=600)
-            fig.update_xaxes(title_text="Threshold")
-            fig.update_yaxes(title_text="Accuracy")
-            fig.write_html(os.path.join(args.storage_path, "plots/accuracy", "acc_curve_" + file[25:-4] + ".html"), include_mathjax='cdn')
-            fig.show()
+            # fig = px.line(x=np.linspace(0, 1, 101), y=acc, title="Accuracy", width=600, height=600)
+            # fig.update_xaxes(title_text="Threshold")
+            # fig.update_yaxes(title_text="Accuracy")
+            # fig.write_html(os.path.join(args.storage_path, "plots/accuracy", "acc_curve_" + file[25:-4] + ".html"), include_mathjax='cdn')
+            # fig.write_image(os.path.join(args.storage_path, "plots/accuracy", "acc_curve_" + file[25:-4] + ".png"))
+            # fig.show()

experiments/conso/get_stats.py
@@ -75,7 +75,7 @@ def get_stats(results, times, storage_path, query=True):
     list_ssim = []
     for file in os.listdir(os.path.join(os.path.dirname(results), "output")):
         result = np.load(os.path.join(os.path.dirname(results), "output", file))
-        tpr, fpr, auc, ssim, acc = get_perf(storage_path, result)
+        tpr, fpr, auc, ssim = get_perf(storage_path, result)
         list_auc.append(auc)
         list_ssim.append(ssim)
     