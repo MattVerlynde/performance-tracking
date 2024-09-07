7f92cb23a050a62165de43664bf80388347777a7
Matthieu <matthieu.verlynde@agroparistech.fr>
2024-09-07 13:09:23 +0200

    support deep learning result analysis

--- experiments/conso_classif_deep/classif_deep.py
+++ experiments/conso_classif_deep/classif_deep.py
@@ -8,16 +8,20 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", '-m', type=str, default='ShortCNN')
-    parser.add_argument("--epochs", '-e', type=int, default=10)
+    parser.add_argument("--epochs", '-e', type=float, default=10)
     parser.add_argument("--optim", '-o', type=str, default='SGD')
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--loss", '-l', type=str, default='CrossEntropy')
-    parser.add_argument("--batch", '-b', type=int, default=256)
-    parser.add_argument("--count", type=int, default=0)
-    parser.add_argument("--rgb", type=int, default=1)
+    parser.add_argument("--batch", '-b', type=float, default=256)
+    parser.add_argument("--count", type=float, default=0)
+    parser.add_argument("--rgb", type=float, default=1)
+    parser.add_argument("--finetune", type=float, default=0)
     parser.add_argument("--storage_path", type=str, required=True)
     args = parser.parse_args()
 
+    args.epochs = int(args.epochs)
+    args.batch = int(args.batch)
+    args.finetune = int(args.finetune)
     args.count = "--count" if args.count else "--no-count"
     args.rgb = "--rgb" if args.rgb else "--no-rgb"
 
--- experiments/conso_classif_deep/simulation_metrics_exec.sh
+++ experiments/conso_classif_deep/simulation_metrics_exec.sh
@@ -7,7 +7,7 @@ touch $2
 t0=$(date -u +%Y-%m-%dT%T.%9NZ)
 echo ${t0} >> $2
 
-sleep 10
+sleep 180
 
 echo $(date -u +%Y-%m-%dT%T.%9NZ) >> $2
 
