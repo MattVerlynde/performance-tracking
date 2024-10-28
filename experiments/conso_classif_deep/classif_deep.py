eae594ed3458d2d66cd7afd6041d4ca0f40db556
Matthieu <matthieu.verlynde@agroparistech.fr>
2024-10-28 14:00:56 +0100

    support training on Inception

--- experiments/conso_classif_deep/classif_deep.py
+++ experiments/conso_classif_deep/classif_deep.py
@@ -16,12 +16,14 @@ if __name__ == "__main__":
     parser.add_argument("--count", type=float, default=0)
     parser.add_argument("--rgb", type=float, default=1)
     parser.add_argument("--finetune", type=float, default=0)
+    parser.add_argument("--seed", type=float, default=42)
     parser.add_argument("--storage_path", type=str, required=True)
     args = parser.parse_args()
 
     args.epochs = int(args.epochs)
     args.batch = int(args.batch)
     args.finetune = int(args.finetune)
+    args.seed = int(args.seed)
     args.count = "--count" if args.count else "--no-count"
     args.rgb = "--rgb" if args.rgb else "--no-rgb"
 
@@ -30,6 +32,6 @@ if __name__ == "__main__":
 
     os.makedirs(os.path.join(args.storage_path, "output"), exist_ok=True)
 
-    command = f"bash performance-tracking/experiments/conso_classif_deep/simulation_metrics_exec.sh {results_path} {times_path} python3 train.py --model {args.model} --epochs {args.epochs} --optim {args.optim} --lr {args.lr} --loss {args.loss} --batch {args.batch} {args.count} {args.rgb} --storage_path {args.storage_path}"
+    command = f"bash performance-tracking/experiments/conso_classif_deep/simulation_metrics_exec.sh {results_path} {times_path} python3 train.py --model {args.model} --epochs {args.epochs} --optim {args.optim} --lr {args.lr} --loss {args.loss} --batch {args.batch} --seed {args.seed} {args.count} {args.rgb} --storage_path {args.storage_path}"
 
     result = subprocess.run(command, shell=True)
