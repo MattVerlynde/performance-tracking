1d941980b5c4d16aca95c102f71a1f8a4d5a0e76
Matthieu <matthieu.verlynde@agroparistech.fr>
2025-06-04 17:39:02 +0200

    refactor sys.path handling for improved module import resolution

--- experiments/conso/get_stats.py
+++ experiments/conso/get_stats.py
@@ -20,7 +20,7 @@ from scipy import integrate
 from skimage.metrics import structural_similarity
 
 import sys
-sys.path.append('/home/verlyndem/Documents/Tests_change_detection/SAR-change-detection/performance-tracking/experiments')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from get_conso import make_table, query_data, filter_time, get_score 
 from conso_change.get_perf import get_perf_change
 from conso_clustering.get_perf import get_perf_clustering
